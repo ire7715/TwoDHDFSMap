054406fa30fb0957cba2781ade8029d508d8aff0
ire_sun <ire7715@hotmail.com>
2016-01-29 11:13:32 +0800

    add key checking by ls

--- TwoDHDFSMap/TwoDHDFSMap.py
+++ TwoDHDFSMap/TwoDHDFSMap.py
@@ -1,9 +1,11 @@
+from hdfs import Config
+
 class TwoDHDFSMap(object):
   def __init__(self, sc, hdfsURI=None, \
     inputFormatClass="org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat", \
     outputFormatClass="org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat", \
     keyClass="org.apache.hadoop.io.Text", valueClass="org.apache.hadoop.io.IntWritable", \
-    outURI=None):
+    outURI=None, hdfsConfigAlias=None):
     self.__hdfsURI = str(hdfsURI) if hdfsURI else None
     self.__sc = sc
     self.__map = dict()
@@ -14,13 +16,20 @@ class TwoDHDFSMap(object):
     self.__ioOptions["valueClass"] = str(valueClass)
     self.__outURI = str(outURI) if outURI else None
 
+    if self.__hdfsURI:
+      self.__client = Config.get_client(hdfsConfigAlias)
+      tempKeys = self.__client.list(self.__hdfsURI + "/") # raise hdfs.util.HdfsError when directory doesn't exist
+      self.__existingKeys = map(lambda direct: direct.split("/")[-1], tempKeys)
+
   @property
   def hdfsURI(self):
       return self.__hdfsURI
 
   def __getitem__(self, key):
+    if "/" in key:
+      raise KeyError("A slash(/) in the key is not allowed.")
     if key not in self.__map:
-      if self.__hdfsURI:
+      if self.__hdfsURI and key in self.__existingKeys:
         try:
           rdd = self.__sc.newAPIHadoopFile(self.__hdfsURI + "/" + str(key), \
             self.__ioOptions["inputFormatClass"], \
